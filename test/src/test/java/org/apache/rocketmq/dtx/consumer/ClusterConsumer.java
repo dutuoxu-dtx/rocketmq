64dc327dbec775f8d6550204f981ecaf78668a5e
dutuoxu <dutuoxu@kinthtech.com>
2025-03-06 16:16:14 +0800

    自己测试的提交

---
+++ test/src/test/java/org/apache/rocketmq/dtx/consumer/ClusterConsumer.java
@@ -0,0 +1,40 @@
+package org.apache.rocketmq.dtx.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+/**
+ * 集群模式（负载均衡）
+ */
+public class ClusterConsumer {
+
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者,指定组名
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("group1");
+        // 指定Namesrv地址信息.
+        consumer.setNamesrvAddr("175.178.54.187:9876");
+        // 订阅Topic
+        consumer.subscribe("Test", "*");
+        //负载均衡模式消费
+        consumer.setMessageModel(MessageModel.CLUSTERING);
+        // 注册回调函数，处理消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n",
+                        Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        //启动消息者
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
---
+++ test/src/test/java/org/apache/rocketmq/dtx/consumer/ScheduledMessageConsumer.java
@@ -0,0 +1,39 @@
+package org.apache.rocketmq.dtx.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * 延时消息<br>
+ * 比如电商里，提交了一个订单就可以发送一个延时消息，1h后去检查这个订单的状态，如果还是未付款就取消订单释放库存。<br>
+ * 启动消息消费者
+ */
+public class ScheduledMessageConsumer {
+
+    public static void main(String[] args) throws Exception {
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ExampleConsumer");
+        // 订阅Topics
+        consumer.subscribe("TestTopic", "*");
+        // 注册消息监听者
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messages, ConsumeConcurrentlyContext
+                    context) {
+                for (MessageExt message : messages) {
+                    // Print approximate delay time period
+                    System.out.println("Receive message[msgId=" + message.getMsgId() + "] " +
+                            (System.currentTimeMillis() - message.getStoreTimestamp()) + "ms later");
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 启动消费者
+        consumer.start();
+    }
+}
